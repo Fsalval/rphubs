1ca1344b5581101fb6f8ac9e31fa51653759258c
fernanda <fernanda.salinas.val@gmail.com>
2025-09-01 18:18:21 -0400

    fix google

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFOR_9CTWIitgM5EYRg34YHKS134lX5tw",
-    authDomain: "rphubs.firebaseapp.com",
-    databaseURL: "https://rphubs-default-rtdb.firebaseio.com",
-    projectId: "rphubs",
-    storageBucket: "rphubs.firebasestorage.app",
-    messagingSenderId: "555326551319",
-    appId: "1:555326551319:web:779b92eee7bd404f2a7352"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Aseguramos que initializeApp() se llame solo una vez
